0f160a49786093cc19b4f4c23294d49167787861
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-09-17 21:41:35 +0900

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@ date_default_timezone_set("Asia/Tokyo");
 
 
 try {
-    $con = new PDO("mysql:dbname=VideoTube;host=localhost", "root", "");
+    $con = new PDO("mysql:dbname=dentechno;host=localhost", "root", "");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     $con->query("SET time_zone = '+09:00'");
     $con->query("SET @@session.time_zone = '+09:00'");
@@ -16,4 +16,19 @@ try {
 catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
+function dbConnect(){
+    $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+    $db['dbname'] = ltrim($db['path'], '/');
+    $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+    $user = $db['user'];
+    $password = $db['pass'];
+    $options = array(
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+    );
+    $dbh = new PDO($dsn,$user,$password,$options);
+    return $dbh;
+  }
 ?>
