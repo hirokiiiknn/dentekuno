9963d1d4c06a2933e8133993b035fd2c0365ca7e
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-09-18 10:48:19 +0900

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -6,9 +6,12 @@ session_start();
 
 date_default_timezone_set("Asia/Tokyo");
 
+$servername = "us-cdbr-east-02.cleardb.com";
+$username = "bafba681bcdc9f";
+$password = "b78e28cb";
 
 try {
-    $con = new PDO("mysql:dbname=dentechno;port=8889;host=localhost", "root", "");
+    $con = new PDO("mysql:dbname=dentechno;port=3032;host=$servername", $username, $password);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     $con->query("SET time_zone = '+09:00'");
     $con->query("SET @@session.time_zone = '+09:00'");
