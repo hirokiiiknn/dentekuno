d40e3c9a015c31858c1280125cb7eb2fb185f5b8
hirokiiiknn <62924651+hirokiiiknn@users.noreply.github.com>
2020-09-18 11:57:08 +0900

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -11,7 +11,7 @@ $username = "bafba681bcdc9f";
 $password = "b78e28cb";
 
 try {
-    $con = new PDO("mysql:dbname=dentechno;port=3036;host=$servername", $username, $password);
+    $con = new PDO("mysql:dbname=dentechno;port=8082;host=$servername", $username, $password);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     $con->query("SET time_zone = '+09:00'");
     $con->query("SET @@session.time_zone = '+09:00'");
